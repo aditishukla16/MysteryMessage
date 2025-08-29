816f41a48f478de2f7c1cb998768f31d84e27d10
Aditi Shukla <aditishukla1676@gmail.com>
2025-08-29 23:42:21 +0530

    emails

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,2 +1,18 @@
 import {resend} from "@/lib/resend";
 import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+    email: string,
+    username:string,
+    verifyCode: string
+):<ApiResponse>{
+    try {
+        return{success: true,message:'Verification email send successfully'}
+        
+    } catch (emailError) {
+        console.error("Error sending verification email",emailError)
+        return{success:false,messafe:"Failed to send verfication email"}
+    }
+}
+    
